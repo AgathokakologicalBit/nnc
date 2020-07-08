4be6aec18a45cf52da54ae95c1d715bddb61e708
Michael Ermishin <ermichin1999@gmail.com>
2020-07-08 21:41:51 +0300

    Created notebook to html converter

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# generates html from jupyter notebook
+jupyter-nbconvert --execute ./ml.ipynb
+
+# opens generated file in the browser (or whatever it can open it with)
+xdg-open ./ml.html
+
